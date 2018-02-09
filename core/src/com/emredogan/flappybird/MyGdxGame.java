5d9012160d6dda36ecbf3f56a8ee8d9954573a3c
Emre <emredogan.boun@gmail.com>
2018-02-10 00:25:21 +0100

    game over screen

--- core/src/com/emredogan/flappybird/MyGdxGame.java
+++ core/src/com/emredogan/flappybird/MyGdxGame.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.GLFrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
@@ -29,6 +31,10 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	Circle birdCircle;
 
+	int score = 0;
+
+	int scoringTube = 0;
+
 	int velocity = 0;
 
 	int gameState = 0;
@@ -37,12 +43,15 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	Texture toptube;
 	Texture bottomtube;
+	Texture gameover;
 
 	float gap = 400;
 
 	float maxTubeOffSet;
 
 	Random randomGenerator;
+
+	BitmapFont font;
 
 
 	float tubeVelocity = 4;
@@ -73,10 +82,15 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		toptube = new Texture("toptube.png");
 		bottomtube = new Texture("bottomtube.png");
+		gameover = new Texture("gameover.png");
 
 		maxTubeOffSet = Gdx.graphics.getHeight() / 2 - gap/2 - 100;
 
 		randomGenerator = new Random();
+
+		font = new BitmapFont();
+		font.setColor(Color.WHITE);
+		font.getData().setScale(10);
 
 		distanceBetweenTubes = Gdx.graphics.getWidth() * 3 / 4;
 
@@ -86,6 +100,19 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		topTubeRectangles = new Rectangle[numberOfTubes];
 		bottomTubeRectangles = new Rectangle[numberOfTubes];
+
+		startGame();
+
+
+
+
+
+
+
+	}
+
+	public void startGame() {
+		birdY = Gdx.graphics.getHeight()/2 - birds[0].getHeight()/2;
 
 		for(int i=0; i< numberOfTubes; i++) {
 
@@ -98,11 +125,6 @@ public class MyGdxGame extends ApplicationAdapter {
 			bottomTubeRectangles[i] = new Rectangle();
 
 		}
-
-
-
-
-
 	}
 
 	@Override
@@ -112,7 +134,19 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.draw(background, 0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
 
-		if (gameState != 0) {
+		if (gameState == 1) {
+
+			if(tubeX[scoringTube] < Gdx.graphics.getWidth() / 2) {
+				score++;
+
+				Gdx.app.log("Score", String.valueOf(score));
+
+				if(scoringTube < numberOfTubes - 1) {
+					scoringTube++;
+				} else {
+					scoringTube = 0;
+				}
+			}
 
 
 			if(Gdx.input.justTouched()) {
@@ -134,6 +168,8 @@ public class MyGdxGame extends ApplicationAdapter {
 
 					tubeX[i] -= tubeVelocity;
 
+
+
 				}
 
 
@@ -150,11 +186,13 @@ public class MyGdxGame extends ApplicationAdapter {
 			}
 
 
-			if (birdY > 0 || velocity < 0) {
+			if (birdY > 0) {
 
 				velocity += gravity;
 				birdY -= velocity;
 
+			} else {
+				gameState = 2;
 			}
 
 
@@ -164,13 +202,30 @@ public class MyGdxGame extends ApplicationAdapter {
 
 
 
-		} else {
+		} else if (gameState == 0) {
 
 
 			if(Gdx.input.justTouched()) {
 
 				gameState = 1;
+
 			}
+		} else if (gameState == 2) {
+
+			batch.draw(gameover, Gdx.graphics.getWidth() / 2 -gameover.getWidth()/2, Gdx.graphics.getHeight() / 2 - gameover.getHeight()/2 );
+
+			if(Gdx.input.justTouched()) {
+
+				gameState = 1;
+
+				startGame();
+				score = 0;
+				scoringTube = 0;
+				velocity = 0;
+
+
+			}
+
 		}
 
 		if (flapState == 0) {
@@ -182,9 +237,14 @@ public class MyGdxGame extends ApplicationAdapter {
 
 
 		batch.draw(birds[flapState],Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2,birdY);
+
+		font.draw(batch,String.valueOf(score), 100,200);
+
 		batch.end();
 
 		birdCircle.set(Gdx.graphics.getWidth() /2, birdY + birds[flapState].getHeight() /2, birds[flapState].getWidth() /2);
+
+
 
 
 
@@ -200,12 +260,13 @@ public class MyGdxGame extends ApplicationAdapter {
 
 			if (Intersector.overlaps(birdCircle,topTubeRectangles[i]) || Intersector.overlaps(birdCircle,bottomTubeRectangles[i])) {
 
-				Gdx.app.log("Collision","YES");
+				gameState = 2;
+
 
 			}
 		}
 
-		shapeRenderer.end();
+	//	shapeRenderer.end();
 
 
 
