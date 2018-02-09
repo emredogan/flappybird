bba89dea90be80972117d0f25791aec76923592c
Emre <emredogan.boun@gmail.com>
2018-02-09 14:55:14 +0100

    interacting with sprites

--- core/src/com/emredogan/flappybird/MyGdxGame.java
+++ core/src/com/emredogan/flappybird/MyGdxGame.java
@@ -9,12 +9,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import sun.rmi.runtime.Log;
+
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 	Texture[] birds;
 
+	float birdY = 0;
+
 	int flapState = 0;
+
+	int velocity = 0;
+
+	int gameState = 0;
+
+	float gravity = 2;
 	
 	@Override
 	public void create () {
@@ -24,13 +34,50 @@ public class MyGdxGame extends ApplicationAdapter {
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
 
+		birdY = Gdx.graphics.getHeight()/2 - birds[0].getHeight()/2;
+
+
+
 
 
 	}
 
 	@Override
 	public void render () {
-		
+
+
+		if (gameState != 0) {
+
+
+
+			if(Gdx.input.justTouched()) {
+
+				velocity = -30;
+
+			}
+
+			if (birdY > 0 || velocity < 0) {
+
+				velocity += gravity;
+				birdY -= velocity;
+
+			}
+
+
+
+
+
+
+
+
+		} else {
+
+
+			if(Gdx.input.justTouched()) {
+
+				gameState = 1;
+			}
+		}
 
 		if (flapState == 0) {
 			flapState = 1;
@@ -40,8 +87,10 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		batch.begin();
 		batch.draw(background, 0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw(birds[flapState],Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2,Gdx.graphics.getHeight()/2 - birds[flapState].getHeight()/2);
+		batch.draw(birds[flapState],Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2,birdY);
 		batch.end();
+
+
 
 	}
 	
